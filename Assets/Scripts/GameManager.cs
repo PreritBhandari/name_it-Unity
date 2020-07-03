9a4e113e25f9a5ebf1a4a613c1470309c93814e7
Nirmal Subedi <nirmal.subedi@outlook.com>
2020-07-02 22:35:37 -0500

    Init on commit on git

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public Button StartButton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartButton?.onClick.AddListener(StartGame);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartGame()
+    {
+        Debug.Log("You have clicked the button!");
+        SceneManager.LoadScene("GameScene");
+    }
+}
---
+++ Assets/Scripts/InputButton.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InputButton : MonoBehaviour
+{
+    public string Alphabet;
+    private Button Button;
+    private Text ButtonLabel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Button = this.GetComponent<Button>();
+        ButtonLabel = Button?.GetComponentInChildren<Text>();
+
+        if(!string.IsNullOrEmpty(Alphabet))
+            ButtonLabel.text = Alphabet;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
