5df62905a0f8f27cb451dd88c3bfe902fe08dd30
Nirmal Subedi <nirmal.subedi@outlook.com>
2020-07-10 17:34:57 -0500

    added the design for levels scene. added some animations as well

--- Assets/_GameScripts/NameITGameManager.cs
+++ Assets/_GameScripts/NameITGameManager.cs
@@ -3,11 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 //This script is attached in Root of the Home Scene!
 
 public class NameITGameManager : MonoBehaviour
 {
+    public Button PlayButton;
+
     public static NameITGameManager Instance { get; private set; }
 
     private void Awake()
@@ -24,6 +28,18 @@ public class NameITGameManager : MonoBehaviour
 
     private void Start()
     {
+        //Initialize Play Button Click:
+        if (PlayButton != null)
+        {
+            Button startButton = PlayButton.GetComponent<Button>();
+            startButton.onClick.AddListener(OpenLevelsScene);
+        }
+
         GameDataManager.Instance.Init();
+    }
+
+    private void OpenLevelsScene()
+    {
+        SceneManager.LoadScene("LevelsScene");
     }
 }
